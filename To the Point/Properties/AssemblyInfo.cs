5867d062795ed66ba52b3803d52595623a288e3d
3Daniel <daniel@dan3d.org>
2018-09-16 10:11:03 +1200

    Updated project name

--- To the Point/Properties/AssemblyInfo.cs
+++ To the Point/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Godot Testing")]
+[assembly: AssemblyTitle("PxlJam2018-ToThePoint")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
